0bd08145a5d1a06b6bbd775d36ad00c14f457f01
Bilal Tahir <btahir@ultimus.com>
2025-12-31 17:53:08 +0500

    Initial commet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./features/dashboard/dashboard.routes')
+        .then(m => m.DASHBOARD_ROUTES),
+  },
+  {
+    path: 'projects',
+    loadChildren: () =>
+      import('./features/projects/projects.routes')
+        .then(m => m.PROJECT_ROUTES),
+  }
+];
---
+++ src/app/shared/models/project.model.ts
@@ -0,0 +1,8 @@
+export interface Project {
+  id: number;
+  name: string;
+  location?: string;
+  status: 'Active' | 'Completed';
+  startDate?: string;
+  endDate?: string;
+}
